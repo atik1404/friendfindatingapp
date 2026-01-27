1d45205a02cfeac223d3701504992fd0b2a15264
Atik Faysal <helloatik.apps@gmail.com>
2026-01-27 23:38:15 +0600

    build(version): Increment versionCode to 156 and versionName to 1.0.156

--- build-logic/convention/src/main/kotlin/com/friendfinapp/AppConfig.kt
+++ build-logic/convention/src/main/kotlin/com/friendfinapp/AppConfig.kt
@@ -9,6 +9,6 @@ object AppConfig {
     const val targetSdkVersion = 36
     var testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     val compatibilityVersion = JavaVersion.VERSION_17
-    const val versionCode  = 155
-    const val versionName  = "1.0.155"
+    const val versionCode  = 156
+    const val versionName  = "1.0.156"
 }
