49e5b7a276d2954fff6a8915c5c0fe32e09ce2ac
Atik Faysal <helloatik.apps@gmail.com>
2026-02-16 23:57:44 +0600

    feat(app): update version code to 166 and version name to 1.0.166

--- build-logic/convention/src/main/kotlin/com/friendfinapp/AppConfig.kt
+++ build-logic/convention/src/main/kotlin/com/friendfinapp/AppConfig.kt
@@ -9,6 +9,6 @@ object AppConfig {
     const val targetSdkVersion = 36
     var testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     val compatibilityVersion = JavaVersion.VERSION_17
-    const val versionCode  = 165
-    const val versionName  = "1.0.165"
+    const val versionCode  = 166
+    const val versionName  = "1.0.166"
 }
